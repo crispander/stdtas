f3daa326d3bee856ebdb98ddf89ea753fe7c5d85
unknown <you@example.com>
2013-07-13 13:13:15 -0500

    cI

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -14,10 +14,10 @@ framework.
 
 """
 import os
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# from django.core.wsgi import get_wsgi_application
+# from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
