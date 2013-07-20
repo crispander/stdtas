b80b6ce77d900b502c2fe227a6f62af9a5cc71ea
unknown <you@example.com>
2013-07-20 18:39:07 -0500

    add

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,25 +16,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-import sys, urlparse
-urlparse.uses_netloc.append('postgres')
-urlparse.uses_netloc.append('mysql')
-try:
-    if os.environ.has_key('DATABASE_URL'):
-        url = urlparse.urlparse(os.environ['DATABASE_URL'])
-        DATABASES['default'] = {
-            'NAME':     url.path[1:],
-            'USER':     url.username,
-            'PASSWORD': url.password,
-            'HOST':     url.hostname,
-            'PORT':     url.port,
-        }
-        if url.scheme == 'postgres':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-        if url.scheme == 'mysql':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-except:
-    print "Unexpected error:", sys.exc_info()
+import dj_database_url
+
+DATABASES = {'default': dj_database_url.config(default=os.environ["HEROKU_POSTGRESQL_BLUE_URL"])}
 
 # DATABASES = {
    # 'default': {
