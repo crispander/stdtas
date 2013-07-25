30f424b5b1cb71c06cd442af5bbfbce518e8da59
unknown <crispander@gmail.com>
2013-07-25 10:46:44 -0500

    se añade nueva onformacion

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,7 +139,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'mysite.polls',
     'tastypie',
-    'south'
+    #'south'
 )
 
 # A sample logging configuration. The only tangible logging
